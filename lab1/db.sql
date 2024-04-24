e6474ca2240ebd50c93c69b6cee08c1d3031dd7e
GektorPestarzt <96793195+GektorPestarzt@users.noreply.github.com>
2024-04-24 14:04:04 +0300

    Update db.sql

--- lab1/db.sql
+++ lab1/db.sql
@@ -1,25 +1,48 @@
 CREATE TABLE kanjis (
-  id INTEGER PRIMARY KEY,
-  kanji VARCHAR UNIQUE,
+  kanji TEXT PRIMARY KEY,
   meaning TEXT,
-  radicals_id INTEGER,
-  consist_kanjis_id INTEGER,
-  readings_id INTEGER,
+  key TEXT,
   strokes INTEGER
+);
+
+CREATE TABLE consist_kanjis (
+  kanji TEXT NOT NULL,
+  consist_kanji TEXT NOT NULL,
+  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
+  FOREIGN KEY (consist_kanji) REFERENCES kanjis (kanji)
+);
+
+CREATE TABLE kanjis_radicals (
+  kanji TEXT NOT NULL,
+  radical TEXT NOT NULL,
+  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
+  FOREIGN KEY (radical) REFERENCES radicals (radical)
+);
+
+CREATE TABLE kanjis_readings (
+  kanji TEXT NOT NULL,
+  reading_id INTEGER NOT NULL,
+  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
+  FOREIGN KEY (reading_id) REFERENCES readings (id)
 );
 
 CREATE TABLE radicals (
-  id INTEGER PRIMARY KEY,
-  radical VARCHAR UNIQUE,
-  readings_id INTEGER,
-  strokes INTEGER
+  radical TEXT PRIMARY KEY,
+  reading_id INTEGER,
+  meaning TEXT,
+  FOREIGN KEY (reading_id) REFERENCES readings (id)
 );
 
 CREATE TABLE words (
-  id INTEGER PRIMARY KEY,
-  word TEXT UNIQUE,
-  meaning TEXT,
-  kanjis_id INTEGER
+  word TEXT PRIMARY KEY,
+  meaning TEXT
+);
+
+CREATE TABLE words_kanjis (
+  word TEXT NOT NULL,
+  kanji TEXT NOT NULL,
+  FOREIGN KEY (word) REFERENCES words (word),
+  FOREIGN KEY (kanji) REFERENCES kanjis (kanji)
 );
 
 CREATE TABLE readings (
@@ -28,69 +51,26 @@ CREATE TABLE readings (
 );
 
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  username TEXT UNIQUE,
-  lists_id INTEGER
+  username TEXT PRIMARY KEY
+);
+
+CREATE TABLE users_lists (
+  username TEXT NOT NULL,
+  list_id INTEGER NOT NULL,
+  FOREIGN KEY (username) REFERENCES users (username),
+  FOREIGN KEY (list_id) REFERENCES lists (id)
 );
 
 CREATE TABLE lists (
   id INTEGER PRIMARY KEY,
-  name TEXT,
-  kanjis_id INTEGER
+  username TEXT,
+  table_name TEXT NOT NULL,
+  description TEXT
 );
 
-CREATE TABLE radicals_kanjis (
-  radicals_id INTEGER,
-  kanjis_radicals_id INTEGER,
-  PRIMARY KEY (radicals_id, kanjis_radicals_id),
-  FOREIGN KEY (radicals_id) REFERENCES radicals (id),
-  FOREIGN KEY (kanjis_radicals_id) REFERENCES kanjis (radicals_id)
-);
-
-CREATE TABLE kanjis_words (
-  kanjis_id INTEGER,
-  words_kanjis_id INTEGER,
-  PRIMARY KEY (kanjis_id, words_kanjis_id),
-  FOREIGN KEY (kanjis_id) REFERENCES kanjis (id),
-  FOREIGN KEY (words_kanjis_id) REFERENCES words (kanjis_id)
-);
-
-CREATE TABLE kanjis_kanjis (
-  kanjis_id INTEGER,
-  kanjis_consist_kanjis_id INTEGER,
-  PRIMARY KEY (kanjis_id, kanjis_consist_kanjis_id),
-  FOREIGN KEY (kanjis_id) REFERENCES kanjis (id),
-  FOREIGN KEY (kanjis_consist_kanjis_id) REFERENCES kanjis (consist_kanjis_id)
-);
-
-CREATE TABLE readings_kanjis (
-  readings_id INTEGER,
-  kanjis_readings_id INTEGER,
-  PRIMARY KEY (readings_id, kanjis_readings_id),
-  FOREIGN KEY (readings_id) REFERENCES readings (id),
-  FOREIGN KEY (kanjis_readings_id) REFERENCES kanjis (readings_id)
-);
-
-CREATE TABLE readings_radicals (
-  readings_id INTEGER,
-  radicals_readings_id INTEGER,
-  PRIMARY KEY (readings_id, radicals_readings_id),
-  FOREIGN KEY (readings_id) REFERENCES readings (id),
-  FOREIGN KEY (radicals_readings_id) REFERENCES radicals (readings_id)
-);
-
-CREATE TABLE kanjis_lists (
-  kanjis_id INTEGER,
-  lists_kanjis_id INTEGER,
-  PRIMARY KEY (kanjis_id, lists_kanjis_id),
-  FOREIGN KEY (kanjis_id) REFERENCES kanjis (id),
-  FOREIGN KEY (lists_kanjis_id) REFERENCES lists (kanjis_id)
-);
-
-CREATE TABLE lists_users (
-  lists_id INTEGER,
-  users_lists_id INTEGER,
-  PRIMARY KEY (lists_id, users_lists_id),
-  FOREIGN KEY (lists_id) REFERENCES lists (id),
-  FOREIGN KEY (users_lists_id) REFERENCES users (lists_id)
+CREATE TABLE lists_kanjis (
+  list_id INTEGER NOT NULL,
+  kanji TEXT NOT NULL,
+  FOREIGN KEY (list_id) REFERENCES lists (id),
+  FOREIGN KEY (kanji) REFERENCES kanjis (kanji)
 );
