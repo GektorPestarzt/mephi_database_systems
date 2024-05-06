557ae6f97d8ed8c75dc7b836531871c428d12809
GektorPestarzt <96793195+GektorPestarzt@users.noreply.github.com>
2024-05-06 10:15:33 +0300

    Add files via upload

--- lab1/db.sql
+++ lab1/db.sql
@@ -2,75 +2,66 @@ CREATE TABLE kanjis (
   kanji TEXT PRIMARY KEY,
   meaning TEXT,
   key TEXT,
+  readings TEXT,
   strokes INTEGER
 );
 
 CREATE TABLE consist_kanjis (
   kanji TEXT NOT NULL,
   consist_kanji TEXT NOT NULL,
-  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
-  FOREIGN KEY (consist_kanji) REFERENCES kanjis (kanji)
-);
-
-CREATE TABLE kanjis_radicals (
-  kanji TEXT NOT NULL,
-  radical TEXT NOT NULL,
-  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
-  FOREIGN KEY (radical) REFERENCES radicals (radical)
-);
-
-CREATE TABLE kanjis_readings (
-  kanji TEXT NOT NULL,
-  reading_id INTEGER NOT NULL,
-  FOREIGN KEY (kanji) REFERENCES kanjis (kanji),
-  FOREIGN KEY (reading_id) REFERENCES readings (id)
-);
-
-CREATE TABLE radicals (
-  radical TEXT PRIMARY KEY,
-  reading_id INTEGER,
-  meaning TEXT,
-  FOREIGN KEY (reading_id) REFERENCES readings (id)
+  FOREIGN KEY (kanji) REFERENCES kanjis(kanji),
+  FOREIGN KEY (consist_kanji) REFERENCES kanjis(kanji)
 );
 
 CREATE TABLE words (
   word TEXT PRIMARY KEY,
+  reading TEXT,
   meaning TEXT
 );
 
 CREATE TABLE words_kanjis (
   word TEXT NOT NULL,
   kanji TEXT NOT NULL,
-  FOREIGN KEY (word) REFERENCES words (word),
-  FOREIGN KEY (kanji) REFERENCES kanjis (kanji)
+  FOREIGN KEY (word) REFERENCES words(word),
+  FOREIGN KEY (kanji) REFERENCES kanjis(kanji)
 );
 
-CREATE TABLE readings (
-  id INTEGER PRIMARY KEY,
-  reading TEXT UNIQUE
-);
-
-CREATE TABLE users (
-  username TEXT PRIMARY KEY
-);
-
-CREATE TABLE users_lists (
+CREATE TABLE users_lists_of_kanjis (
   username TEXT NOT NULL,
   list_id INTEGER NOT NULL,
-  FOREIGN KEY (username) REFERENCES users (username),
-  FOREIGN KEY (list_id) REFERENCES lists (id)
+  FOREIGN KEY (list_id) REFERENCES lists_of_kanjis(id)
 );
 
-CREATE TABLE lists (
+CREATE TABLE lists_of_kanjis (
   id INTEGER PRIMARY KEY,
   username TEXT,
   table_name TEXT NOT NULL,
   description TEXT
 );
 
+CREATE TABLE lists_of_words (
+  id INTEGER PRIMARY KEY,
+  username TEXT,
+  table_name TEXT NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE lists_words (
+  list_id INTEGER NOT NULL,
+  word TEXT NOT NULL,
+  FOREIGN KEY (list_id) REFERENCES lists_of_words(id),
+  FOREIGN KEY (word) REFERENCES words(word)
+);
+
 CREATE TABLE lists_kanjis (
   list_id INTEGER NOT NULL,
   kanji TEXT NOT NULL,
-  FOREIGN KEY (list_id) REFERENCES lists (id),
-  FOREIGN KEY (kanji) REFERENCES kanjis (kanji)
+  FOREIGN KEY (list_id) REFERENCES lists_of_kanjis(id),
+  FOREIGN KEY (kanji) REFERENCES kanjis(kanji)
+);
+
+CREATE TABLE users_lists_of_words (
+  username TEXT NOT NULL,
+  list_id INTEGER NOT NULL,
+  FOREIGN KEY (list_id) REFERENCES lists_of_words(id)
 );
